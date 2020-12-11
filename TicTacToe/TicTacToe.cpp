68e818811250b8df22be5638e97cbe042abb2bec
Josiah Hartley <josiah.t.hartley@gmail.com>
2020-12-11 09:38:21 -0600

    Finished Comments

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -66,8 +66,10 @@ int getNumber()
 
 void getName(char name[])
 {
+	//Asks user to input name, and then also checks if the user made a mistake
 	std::cout << "Please Enter Your Name" << std::endl;
 	bool correct = 0;
+	// 0 = false = null
 	while (correct == 0)
 	{
 		for (int i = 0; i < 5; i++)
@@ -106,6 +108,7 @@ bool checkPositionAvailable(char grid[3][3], int position)
 
 bool checkWinner(char grid[3][3], char symbol)
 {
+	//Checks to see if all symbols are the same across rows 0,1,2 and columns, 0,1,2 and checks the two diagnals
 	if ((grid[0][0] == symbol && grid[0][1] == symbol && grid[0][2] == symbol) ||
 		(grid[1][0] == symbol && grid[1][1] == symbol && grid[1][2] == symbol) ||
 		(grid[2][0] == symbol && grid[2][1] == symbol && grid[2][2] == symbol) ||
@@ -122,6 +125,7 @@ bool checkWinner(char grid[3][3], char symbol)
 
 bool checkCatEyes(char grid[3][3], char symbol)
 {
+	//if all the positions are filled safe to assume no winner
 	if (grid[0][0] != '1' && grid[0][1] != '2' && grid[0][2] != '3'
 		&& grid[1][0] != '4' && grid[1][1] != '5' && grid[1][2] != '6'
 		&& grid[2][0] != '7' && grid[2][1] != '8' && grid[2][2] != '9')
@@ -133,6 +137,7 @@ bool checkCatEyes(char grid[3][3], char symbol)
 
 void start(char name1[], char name2[])
 {
+	//Allows players to enter their names to give a sense of reference
 	std::cout << "Player One Please Enter Your Name" << std::endl;
 	getName(name1);
 	std::cout << "Player Two Please Enter Your Name" << std::endl;
@@ -141,6 +146,7 @@ void start(char name1[], char name2[])
 
 void draw(char grid[3][3], char name1[], char name2[])
 {
+	//Prints out the Grid spaced out
 	system("cls");
 	for (int i = 0; i < 3; i++)
 	{
@@ -160,19 +166,25 @@ int update(char grid[3][3], char name1[], char name2[])
 	bool p1Loop = true;
 	int p1Position = 0;
 	std::cout << name1 << " please choose a spot to place your piece [X]" << std::endl;
+	//Until player 1 chooses a spot
 	while (p1Loop == true)
 	{
+		//Player can input the number (1-9)
 		p1Position = getNumber();
+		//checkPosition will see if a symbol already is in that spot
 		bool availablePosition = checkPositionAvailable(grid, p1Position);
 		if (availablePosition)
 		{
+			//if its available we're done here
 			p1Loop = false;
 		}
 		else
 		{
+			//if its not the player needs to choose a different spot
 			std::cout << "Please Select A Valid Spot" << std::endl;
 		}
 	}
+	//Conversion for char(1-9) into the [0-2][0-2]
 	int p1Column = -1;
 	int p1Row = 0;
 	for (int i = 0; i < p1Position; i++)
@@ -187,6 +199,7 @@ int update(char grid[3][3], char name1[], char name2[])
 		}
 	}
 	char p1Symbol = 'X';
+	//sets the symbol above to the position on the grid
 	grid[p1Row][p1Column] = p1Symbol;
 
 	if (checkWinner(grid, p1Symbol))
@@ -198,12 +211,14 @@ int update(char grid[3][3], char name1[], char name2[])
 		return 3;
 	}
 	draw(grid, name1, name2);
-	//Player Twos Turn
+	//Player Twos Turn Exact same to player one
 	bool p2Loop = true;
 	int p2Position = 0;
 	std::cout << name2 << " please choose a spot to place your piece [O]" << std::endl;
+	//Until the player gives a valid position to place their symbol
 	while (p2Loop == true)
 	{
+		//Get the number the player wants its piece to go to
 		p2Position = getNumber();
 		bool availablePosition = checkPositionAvailable(grid, p2Position);
 		if (availablePosition)
@@ -245,14 +260,21 @@ int update(char grid[3][3], char name1[], char name2[])
 
 int main()
 {
+	//Main variables needed for game Loop
 	bool loop = true;
     char playerOnesName[5] = { 'N','A','M','E','!' };
     char playerTwosName[5] = { 'N','A','M','E','!' };
 	char grid[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+
+	//Gets both Players Names
 	start(playerOnesName, playerTwosName);
+
+	//loops forever since there is no case to break loop
 	while (loop)
 	{
+		//Winner = to what update returns, 1 for player 1, 2 for player 2, 3 for a tie
 		int winner = update(grid, playerOnesName, playerTwosName);
+
 		if(winner == 1)
 		{
 			system("cls");
@@ -273,9 +295,13 @@ int main()
 		}
 		if (winner == 1 || winner == 2 || winner == 3)
 		{
+			//resets the board
 			for (int i = 0; i < 3; i++)
 				for (int j = 0; j < 3; j++)
 					grid[i][j] = (char)((i * 3) + j + 49);
+							//grid at each index moving left to right top to bottom on grid sets corresponding number
+			//example grid[2][1] is the bottom left corner or 7, using math above (2 * 3) + 0 = 6 + 49 = 55 which is ascii dec for 7
+			//ascii dex 48 = 0 but we want the top left to be 1 not 0
 		}
 	}
 }
